d224d806a8b7555e7855920d3880b363743a63ee
abderrahim assouane <aaswan048@gmail.com>
2023-10-02 19:29:56 +0100

    setup gh-pages

--- src/components/userspace/Sim_management_coms/Basic_setting.jsx
+++ src/components/userspace/Sim_management_coms/Basic_setting.jsx
@@ -157,7 +157,7 @@ const Basic_setting = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     class="w-6 h-6"
                   >
